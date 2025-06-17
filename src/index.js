3b7e7e90ea6e17a3cd4d341b6a2a37ed0373ef71
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-06-17 12:22:38 +0200

    Dev: Finishing the offers post route

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
   res.send('Welcome on my portfolio api !');
 })
 
+app.post('/offers', (req, res) => {
+  const data = req.body;
+  if(data) {
+    // Save the actual offer inside the dedicated database
+
+    res.json({ message: `Current offer has been successfully saved`, offer: data});
+  } else {
+    res.statusCode = 401;
+    res.json({ message: 'The current offer is empty' })
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Your application is running on port : ${PORT}`)
 })
