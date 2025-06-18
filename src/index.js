68a00b743be71c194ba39f9bfc5fe518ad6b684a
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-06-18 06:53:11 +0200

    Dev: Connecting the server with the database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
 const PORT = process.env.PORT || 3000
@@ -17,10 +18,28 @@ app.post('/offers', (req, res) => {
     res.json({ message: `Current offer has been successfully saved`, offer: data});
   } else {
     res.statusCode = 401;
-    res.json({ message: 'The current offer is empty' })
+    res.json({ message: 'The current offer is empty' });
   }
 })
 
-app.listen(PORT, () => {
-  console.log(`Your application is running on port : ${PORT}`)
+// Preparing the db connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'gillesng_dev',
+  password: ']q&C3)}U)sb0',
+  database: 'gillesng_portfolio'
+});
+
+
+// Making the connection with the database
+db.connect((error) => {
+  if(error) {
+    console.error('An error occured while connectiong to the database : ', error.message);
+  } else {
+    // Running the server
+    app.listen(PORT, () => {
+      console.log(`Your application is running on port : ${PORT}`);
+    })
+  }
 })
+  
