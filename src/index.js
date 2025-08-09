21284e074ec6cc80097081d4c26939e2405ac8cb
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-08-09 08:24:45 +0200

    Dev: Adding Resend package for emails

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
+const { Resend } = require('resend');
 const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
 const PORT = process.env.PORT || 3000
-
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,7 +18,8 @@ app.get('/', (_, res) => {
   res.send('Welcome on my portfolio api !');
 })
 
-app.post('/offers', (req, res) => {
+app.post('/offers', async (req, res) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const data = req.body;
   if(data) {
     // Save the actual offer inside the dedicated database
@@ -30,7 +32,16 @@ app.post('/offers', (req, res) => {
     });
 
     // Send the email to the owner of the portfolio
+    const response = await resend.emails.send({
+      from: 'gilles@api.gillesngassam.com',
+      to: 'nanmegningassam@gmail.com',
+      subject: data['subject'],
+      html: data['body'],
+    });
 
+    if(response.error) {
+      res.status(501).json({ message: "The email wasn't delivered!", error: response.error })
+    }
   } else {
     res.status(401).json({ message: 'The current offer is empty' });
   }
@@ -38,17 +49,21 @@ app.post('/offers', (req, res) => {
 
 // Preparing the db connection
 const db = mysql.createConnection({
-  host: process.env.HOST_URL,
-  user: process.env.USER_NAME,
+  host: 'localhost',
+  user: 'gillesng_dev',
   password: process.env.USER_PASSWORD,
-  database: process.env.DATABASE_NAME
+  database: 'gillesng_portfolio'
 });
 
+
+app.listen(PORT, () => {
+  console.log(`Your application is running on port : ${PORT}`);
+})
 
 // Making the connection with the database
 db.connect((error) => {
   if(error) {
-    console.error('An error occured while connectiong to the database : ', error.message);
+    console.error('An error occured while connectiong to the database : ', error);
   } else {
     // Running the server
     app.listen(PORT, () => {
